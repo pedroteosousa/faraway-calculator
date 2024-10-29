a7617d12bc796c18afc7f471e0f81f6966476cef
Pedro Sousa <pedroteosousa@gmail.com>
2024-10-29 09:47:29 -0300

    Initial Commit

---
+++ Faraway Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Faraway Calculator
+//
+//  Created by Pedro Sousa on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Faraway Calculator/Faraway_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Faraway_CalculatorApp.swift
+//  Faraway Calculator
+//
+//  Created by Pedro Sousa on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Faraway_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
